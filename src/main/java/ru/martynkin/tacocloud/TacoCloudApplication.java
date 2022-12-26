224c9dcbe2065d65cf8190499a4809f09d60cb29
Igor <imartynkin@yandex.ru>
2022-12-26 23:32:31 +0900

    create Order Controller

--- src/main/java/ru/martynkin/tacocloud/TacoCloudApplication.java
+++ src/main/java/ru/martynkin/tacocloud/TacoCloudApplication.java
@@ -3,6 +3,10 @@ package ru.martynkin.tacocloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Martynkin Igor
+ * <p> Это пет проект на основе книги "Spring в действии. шестое издание - автор Крейг Уоллс</p>
+ */
 @SpringBootApplication
 public class TacoCloudApplication {
 
