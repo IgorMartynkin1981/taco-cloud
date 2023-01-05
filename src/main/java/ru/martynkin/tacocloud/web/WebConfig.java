813adc10c2b21251600dfd0f2cc892309b0c0445
Igor <imartynkin@yandex.ru>
2023-01-05 16:23:19 +0900

    Finished branch add-jdbcData

--- src/main/java/ru/martynkin/tacocloud/web/WebConfig.java
+++ src/main/java/ru/martynkin/tacocloud/web/WebConfig.java
@@ -15,23 +15,4 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
     }
-
-    @Bean
-    public CommandLineRunner dataLoader(IngredientRepository ingredientRepository) {
-        return new CommandLineRunner() {
-            @Override
-            public void run(String... args) throws Exception {
-                ingredientRepository.save(new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
-                ingredientRepository.save(new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP));
-                ingredientRepository.save(new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
-                ingredientRepository.save(new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN));
-                ingredientRepository.save(new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES));
-                ingredientRepository.save(new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES));
-                ingredientRepository.save(new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE));
-                ingredientRepository.save(new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE));
-                ingredientRepository.save(new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
-                ingredientRepository.save(new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
-            }
-        };
-    }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -32,7 +32,7 @@ create table if not exists Ingredient (
   type varchar(10) not null
 );
 
-
+ALTER TABLE Ingredient ADD PRIMARY KEY(ID);
 alter table Taco
     add foreign key (taco_order) references Taco_Order(id);
 alter table Ingredient_Ref
