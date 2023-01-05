b45aae5541a0fae3d19648fcc052faaa871b1d5d
Igor <imartynkin@yandex.ru>
2023-01-05 16:37:36 +0900

    Finished branch add-jdbcData

--- src/main/java/ru/martynkin/tacocloud/web/WebConfig.java
+++ src/main/java/ru/martynkin/tacocloud/web/WebConfig.java
@@ -1,12 +1,8 @@
 package ru.martynkin.tacocloud.web;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import ru.martynkin.tacocloud.Ingredient;
-import ru.martynkin.tacocloud.data.IngredientRepository;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
