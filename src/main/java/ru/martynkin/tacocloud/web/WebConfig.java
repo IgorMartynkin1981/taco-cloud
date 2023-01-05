74583b555ac3f5a26cc780acc4e5422370699e2a
Igor <imartynkin@yandex.ru>
2023-01-05 16:19:56 +0900

    Finished branch add-jdbcData

--- src/main/java/ru/martynkin/tacocloud/web/WebConfig.java
+++ src/main/java/ru/martynkin/tacocloud/web/WebConfig.java
@@ -1,8 +1,12 @@
 package ru.martynkin.tacocloud.web;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import ru.martynkin.tacocloud.Ingredient;
+import ru.martynkin.tacocloud.data.IngredientRepository;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -10,5 +14,24 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
+    }
+
+    @Bean
+    public CommandLineRunner dataLoader(IngredientRepository ingredientRepository) {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                ingredientRepository.save(new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
+                ingredientRepository.save(new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP));
+                ingredientRepository.save(new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
+                ingredientRepository.save(new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN));
+                ingredientRepository.save(new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES));
+                ingredientRepository.save(new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES));
+                ingredientRepository.save(new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE));
+                ingredientRepository.save(new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE));
+                ingredientRepository.save(new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE));
+                ingredientRepository.save(new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE));
+            }
+        };
     }
 }
