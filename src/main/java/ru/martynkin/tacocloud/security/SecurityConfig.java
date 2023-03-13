fb84d8ee75979bf16b3833ac120c96a20f543ca8
Igor <imartynkin@yandex.ru>
2023-03-13 22:21:22 +0900

    Created branch from add-SpringSecurity

--- src/main/java/ru/martynkin/tacocloud/security/SecurityConfig.java
+++ src/main/java/ru/martynkin/tacocloud/security/SecurityConfig.java
@@ -1,50 +1,63 @@
 package ru.martynkin.tacocloud.security;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import ru.martynkin.tacocloud.User;
 import ru.martynkin.tacocloud.data.UserRepository;
 
 @Configuration
-@RequiredArgsConstructor
 public class SecurityConfig {
-
-    private final UserRepository userRepo;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    /**
-     * Определение учетных записей для служебных записей памяти
-     */
-//    @Bean
-//    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-//        List<UserDetails> usersList = new ArrayList<>();
-//        usersList.add(
-//                new User("buzz", encoder.encode("password"),
-//                        List.of((new SimpleGrantedAuthority("ROLE_USER")))));
-//        usersList.add(
-//                new User("woody", encoder.encode("password"),
-//                        List.of((new SimpleGrantedAuthority("ROLE_USER")))));
-//        return new InMemoryUserDetailsManager(usersList);
-//    }
-
-    /**
-     * Определение учетных записей через службу хранения записей
-     */
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepo) {
         return username -> {
             User user = userRepo.findByUsername(username);
-            if (user != null) return user;
-            throw new UsernameNotFoundException("User '" + username + "' not found");
+            if (user != null) {
+                return user;
+            }
+            throw new UsernameNotFoundException(
+                    "User '" + username + "' not found");
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests()
+                .mvcMatchers("/design", "/orders").hasRole("USER")
+                .anyRequest().permitAll()
+
+                .and()
+                .formLogin()
+                .loginPage("/login")
+
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+
+                // Make H2-Console non-secured; for debug purposes
+                .and()
+                .csrf()
+                .ignoringAntMatchers("/h2-console/**")
+
+                // Allow pages to be loaded in frames from the same origin; needed for H2-Console
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+                .build();
     }
 }
